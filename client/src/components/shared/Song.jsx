cd443f22caa0c8d30c38afebc351b3ee5807b301
khimarra <tsolbrig87@gmail.com>
2020-08-23 17:05:23 -0400

    more base styles

--- client/src/components/shared/Song.jsx
+++ client/src/components/shared/Song.jsx
@@ -4,10 +4,10 @@ const Song = (props) => {
   let song = props.song
 
   return (
-    <div>
-      <div>{song.name}</div>
-      <div>{song.song}</div>
-      <div>{song.url}</div>
+    <div className='flex flex-row m-1 mx-2 bg-gray-600'>
+      <div className='w-1/8 px-4'>{song.id}</div>
+      <div className='w-1/4'>{song.name}</div>
+      <a href={song.url} className='w-auto'>{song.song}</a>
     </div>
   )
 }
