9395c911eea49b0d9555cebdf7e2f3e5871ec9fb
khimarra <tsolbrig87@gmail.com>
2020-09-03 12:06:45 -0400

    creating new room

--- client/src/components/screens/Home.jsx
+++ client/src/components/screens/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import { useHistory } from "react-router-dom"
+import { useHistory, Link } from "react-router-dom"
 
 import { getRooms } from "../../services/apiHelper"
 
@@ -23,7 +23,6 @@ const Home = (props) => {
 
     for (let i = 0; i < rooms.length; i++) {
       if (rooms[i].code === input) {
-        console.log(e.target.value)
         history.push(`/room/${rooms[i].id}/${rooms[i].code}`)
       } else {
         let invalid = "Invalid Room Code"
@@ -32,14 +31,23 @@ const Home = (props) => {
     }
   }
 
+  // attempting to make new room
+  // home page has button to go to new room page
+  // new room page has form to fill out
+  // form on new room page has submit to create new room
+
+
+
   return (
-    <div className="home">
+    <div>
       <div className="text-6xl text-center text-blue-400 pt-64">
         Karaoke Night
       </div>
 
-      <form className="flex flex-row justify-center" onSubmit={handleSubmit}>
-        {/* <div className="text-blue-400 text-xl">Join a room:</div> */}
+      <form
+        className="mt-24 flex flex-row justify-center"
+        onSubmit={handleSubmit}
+      >
         <input
           className="p-6 w-40 rounded-md m-2 border-gray-500 border-t-4 border-r border-b border-l-4"
           name="code"
@@ -54,6 +62,15 @@ const Home = (props) => {
           value="Join a room"
         />
       </form>
+
+      <div className="flex mt-24">
+        <Link
+          className="p-6 rounded-md bg-blue-700 text-blue-200 border-gray-800 border-t border-r-4 border-b-4 border-l mx-auto"
+          to="/newroom"
+        >
+          <div>Make a New Room</div>
+        </Link>
+      </div>
     </div>
   )
 }
--- client/src/components/screens/NewRoom.jsx
+++ client/src/components/screens/NewRoom.jsx
@@ -1,9 +1,67 @@
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { createRoom } from '../../services/apiHelper'
 
 const NewRoom = (props) => {
+  const [newRoom, setNewRoom] = useState({
+    title: '',
+    url: '',
+    code: ''
+  })
+
+  const createNewRoom = async () => {
+    const res = await createRoom()
+    setNewRoom(res)
+  }
+
+  useEffect(() => {
+    createNewRoom()
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const room = newRoom
+  }
+
+// const handleSubmit = (e) => {
+//   e.preventDefault()
+
+//   for (let i = 0; i < rooms.length; i++) {
+//     if (rooms[i].code === input) {
+//       history.push(`/room/${rooms[i].id}/${rooms[i].code}`)
+//     } else {
+//       let invalid = "Invalid Room Code"
+//       return invalid
+//     }
+//   }
+// }
+
   return (
-    <div>
-      Hello from New Room
+    <div className="pt-32">
+      <h1 className="text-6xl text-center text-blue-400">Make a New Room</h1>
+      <p className="text-2xl text-center text-blue-500 pt-12 w-3/4 mx-auto">
+        Provide a title and a zoom link for your room. You will be given a 4
+        digit code to share with your friends!
+      </p>
+      <form className="flex flex-col pt-20" onSubmit={handleSubmit}>
+        <input
+          className="p-3 w-64 rounded-md mx-auto my-4 border-gray-500 border-t-4 border-r border-b border-l-4"
+          type="text"
+          placeholder="Room Title"
+          onChange={(e) => setNewRoom(e.target.value)}
+        />
+        <input
+          className="p-3 w-64 rounded-md mx-auto my-4 border-gray-500 border-t-4 border-r border-b border-l-4"
+          type="text"
+          placeholder="Zoom Link"
+        />
+        <input
+          className="p-6 rounded-md bg-blue-700 text-blue-200 border-gray-800 border-t border-r-4 border-b-4 border-l mx-auto mt-10"
+          type="submit"
+          value="Create Room"
+        />
+      </form>
     </div>
   )
 }
